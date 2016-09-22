19cccac78cd0ca6d2f3ae5842aa1acee3be856fc
Liron <liron.hayman@gmail.com>
2016-09-23 01:45:07 +0300

    changed url patterns

--- clinical_trials/trials/urls.py
+++ clinical_trials/trials/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 
-from .views import TrialViewSet
+from .views import TrialViewSet, TrialIndivViewSet
 
 from rest_framework import routers, serializers, viewsets
 
 router = routers.DefaultRouter()
-router.register(r'trials', TrialViewSet)
+router.register(r'trials/many', TrialViewSet)
+router.register(r'trials/individual', TrialIndivViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
--- clinical_trials/trials/views.py
+++ clinical_trials/trials/views.py
@@ -6,17 +6,19 @@ from .serializers import TrialSerializer
 from rest_framework import viewsets
 
 
-def index(request):
-    hot_trials = Trial.objects.order_by('-rank')[:5]
-    output = ', '.join([t.nct_number for t in hot_trials])
-    return HttpResponse(output)
-
-
 # ViewSets define the view behavior.
 class TrialViewSet(viewsets.ModelViewSet):
+    queryset = Trial.objects.all()[:10]
+    serializer_class = TrialSerializer
+
+
+class TrialIndivViewSet(viewsets.ModelViewSet):
     queryset = Trial.objects.all()
     serializer_class = TrialSerializer
     lookup_field = 'nct_number'
+
+
+
 
 
 
